be7b1d2d18c29d3cf7472303e0803ff6628f9ed5
Renaat De Muynck <renaat.demuynck@arteveldehs.be>
2020-04-02 15:58:01 +0200

    Only apply default settings on new conferences

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ export default function ({ router, dom }) {
 
         dom.onElementAdded('#web_conference_user_settings_record', recordSetting => {
             let longRunningSetting = document.getElementById('web_conference_long_running');
+            let form = recordSetting.closest('form');
+            let editMode = form.action.match(/\/courses\/\d+\/conferences\/\d+$/) !== null;
+
+            if (editMode) return;
 
             recordSetting.checked = true;
             longRunningSetting.checked = true;

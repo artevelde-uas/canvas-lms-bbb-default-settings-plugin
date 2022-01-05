e04d79c7f1b2a1194c7e91b41aa20af85095a335
Renaat De Muynck <renaat.demuynck@arteveldehs.be>
2022-01-05 15:59:01 +0100

    Use `const` instead of `let`

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import { router, dom } from '@artevelde-uas/canvas-lms-app';
 export default function () {
     router.onRoute('courses.conferences', params => {
         dom.onElementAdded('#web_conference_user_settings_record', recordSetting => {
-            let longRunningSetting = document.getElementById('web_conference_long_running');
-            let form = recordSetting.closest('form');
-            let editMode = form.action.match(/\/courses\/\d+\/conferences\/\d+$/) !== null;
+            const longRunningSetting = document.getElementById('web_conference_long_running');
+            const form = recordSetting.closest('form');
+            const editMode = form.action.match(/\/courses\/\d+\/conferences\/\d+$/) !== null;
 
             if (editMode) return;
 

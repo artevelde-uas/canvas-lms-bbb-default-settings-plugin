15c00c8c720682c1e39549a70b84a620b692e400
Renaat De Muynck <renaat.demuynck@arteveldehs.be>
2022-01-08 23:08:01 +0100

    Better detection of edit mode

--- src/index.js
+++ src/index.js
@@ -2,13 +2,10 @@ import { router, dom } from '@artevelde-uas/canvas-lms-app';
 
 
 export default function () {
-    router.onRoute('courses.conferences', params => {
-        dom.onElementAdded('#web_conference_user_settings_record', recordSetting => {
+    router.onRoute('courses.conferences', async ({ courseId }) => {
+        dom.onElementAdded(`.ui-dialog form[action="/courses/${courseId}/conferences"]`, form => {
+            const recordSetting = document.getElementById('web_conference_user_settings_record');
             const longRunningSetting = document.getElementById('web_conference_long_running');
-            const form = recordSetting.closest('form');
-            const editMode = form.action.match(/\/courses\/\d+\/conferences\/\d+$/) !== null;
-
-            if (editMode) return;
 
             recordSetting.click();
             longRunningSetting.click();

973f4e4e7fd33c7a31950152e44d15f0f2e161a5
Renaat De Muynck <renaat.demuynck@arteveldehs.be>
2022-01-09 14:31:10 +0100

    Hide conference type selection since tool is now only for BBB

--- src/index.js
+++ src/index.js
@@ -6,16 +6,25 @@ export default function ({
     noTimeLimit = false
 }) {
     router.onRoute('courses.conferences', async ({ courseId }) => {
-        dom.onElementAdded(`.ui-dialog form[action="/courses/${courseId}/conferences"]`, form => {
+        dom.onElementAdded(`.ui-dialog form[action^="/courses/${courseId}/conferences"]`, form => {
+            const conferenceType = document.getElementById('web_conference_conference_type');
+            const editMode = form.action.match(/\/\d+$/) !== null;
+
+            // Hide conference type selection since tool is now only for BBB
+            conferenceType.closest('.control-group').toggleAttribute('hidden', true);
+
+            // Don't change settings when in edit mode
+            if (editMode) return;
+
             const recordSetting = document.getElementById('web_conference_user_settings_record');
             const longRunningSetting = document.getElementById('web_conference_long_running');
 
             if (enableRecording) {
-            recordSetting.click();
+                recordSetting.click();
             }
 
             if (noTimeLimit) {
-            longRunningSetting.click();
+                longRunningSetting.click();
             }
         });
     });

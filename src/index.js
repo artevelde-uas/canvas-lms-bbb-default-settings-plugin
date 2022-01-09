a4d0ec528cb0d172369e0f64768aa13465dad794
Renaat De Muynck <renaat.demuynck@arteveldehs.be>
2022-01-09 14:07:59 +0100

    Make options

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 import { router, dom } from '@artevelde-uas/canvas-lms-app';
 
 
-export default function () {
+export default function ({
+    enableRecording = false,
+    noTimeLimit = false
+}) {
     router.onRoute('courses.conferences', async ({ courseId }) => {
         dom.onElementAdded(`.ui-dialog form[action="/courses/${courseId}/conferences"]`, form => {
             const recordSetting = document.getElementById('web_conference_user_settings_record');
             const longRunningSetting = document.getElementById('web_conference_long_running');
 
+            if (enableRecording) {
             recordSetting.click();
+            }
+
+            if (noTimeLimit) {
             longRunningSetting.click();
+            }
         });
     });
 }

dfced83b65f9cebe40bb7d9b115361f807ada1ce
Renaat De Muynck <renaat.demuynck@arteveldehs.be>
2023-04-25 15:04:55 +0200

    Refactor translation function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { router, dom } from '@artevelde-uas/canvas-lms-app';
 
-import __ from './i18n';
+import t from './i18n';
 
 
 export default function ({
@@ -49,7 +49,7 @@ export default function ({
 
     return {
         ...require('../package.json'),
-        title: __('package.title'),
-        description: __('package.description')
+        title: t('package.title'),
+        description: t('package.description')
     };
 }
